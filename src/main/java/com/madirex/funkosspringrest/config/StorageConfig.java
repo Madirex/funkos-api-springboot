3175e5d01ab966132a59a47bc6b51294f3777a12
Madirex <madirexland@gmail.com>
2023-11-02 20:06:30 +0100

    added Storage Config

---
+++ src/main/java/com/madirex/funkosspringrest/config/StorageConfig.java
@@ -0,0 +1,27 @@
+package com.madirex.funkosspringrest.config;
+
+import com.madirex.funkosspringrest.services.storage.StorageService;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class StorageConfig {
+    @Autowired
+    private StorageService storageService;
+    
+    @Value("${upload.delete}")
+    private String deleteAll;
+
+    @PostConstruct
+    public void init() {
+        if (deleteAll.equals("true")) {
+            log.info("Borrando ficheros de almacenamiento...");
+            storageService.deleteAll();
+        }
+        storageService.init();
+    }
+}
