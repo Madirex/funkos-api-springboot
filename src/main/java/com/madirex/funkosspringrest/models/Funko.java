31da79b907dd825482d26bcbb393870ff0756a1c
Madirex <madirexland@gmail.com>
2023-11-02 20:34:13 +0100

    added IMAGE DEFAULT const to Funko

--- src/main/java/com/madirex/funkosspringrest/models/Funko.java
+++ src/main/java/com/madirex/funkosspringrest/models/Funko.java
@@ -24,6 +24,7 @@ import java.util.UUID;
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 public class Funko {
+    public static final String IMAGE_DEFAULT = "https://www.madirex.com/favicon.ico";
     @NotNull
     @Id
     private UUID id;
