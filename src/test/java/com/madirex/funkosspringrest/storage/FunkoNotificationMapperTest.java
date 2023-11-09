4c58400baa94cd6a9f39dcc07e038a9e34f20a6c
Madirex <madirexland@gmail.com>
2023-11-09 01:34:04 +0100

    added notification mapper test

---
+++ src/test/java/com/madirex/funkosspringrest/storage/FunkoNotificationMapperTest.java
@@ -0,0 +1,52 @@
+package com.madirex.funkosspringrest.storage;
+
+import com.madirex.funkosspringrest.dto.funko.GetFunkoDTO;
+import com.madirex.funkosspringrest.mappers.notification.FunkoNotificationMapper;
+import com.madirex.funkosspringrest.models.Category;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FunkoNotificationMapperTest {
+
+    private FunkoNotificationMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        mapper = new FunkoNotificationMapper();
+    }
+
+    @Test
+    void testToFunkoNotificationDto() {
+        GetFunkoDTO dto = GetFunkoDTO.builder()
+                .id(UUID.randomUUID())
+                .name("nombre")
+                .price(2.2)
+                .quantity(2)
+                .image("imagen")
+                .category(Category.builder()
+                        .id(1L)
+                        .type(Category.Type.MOVIE)
+                        .active(true)
+                        .build())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+        var result = mapper.toFunkoNotificationDto(dto);
+        assertAll("FunkoNotificationResponse properties",
+                () -> assertEquals(dto.getId().toString(), result.id(), "El ID debe coincidir"),
+                () -> assertEquals(dto.getName(), result.name(), "El nombre debe coincidir"),
+                () -> assertEquals(dto.getPrice(), result.price(), "El precio debe coincidir"),
+                () -> assertEquals(dto.getQuantity(), result.quantity(), "La cantidad debe coincidir"),
+                () -> assertEquals(dto.getImage(), result.image(), "La imagen debe coincidir"),
+                () -> assertEquals(dto.getCategory().toString(), result.category(), "La categoría debe coincidir"),
+                () -> assertEquals(dto.getCreatedAt().toString(), result.createdAt(), "La fecha de creación debe coincidir"),
+                () -> assertEquals(dto.getUpdatedAt().toString(), result.updatedAt(), "La fecha de actualización debe coincidir")
+        );
+    }
+}
