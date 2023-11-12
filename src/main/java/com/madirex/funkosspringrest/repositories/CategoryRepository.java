4b095821cda1236805d02eb21b322ae000edd630
Madirex <madirexland@gmail.com>
2023-11-12 21:49:14 +0100

    edited repositories (implementation JpaSpecificationExecutor)

--- src/main/java/com/madirex/funkosspringrest/repositories/CategoryRepository.java
+++ src/main/java/com/madirex/funkosspringrest/repositories/CategoryRepository.java
@@ -2,12 +2,13 @@ package com.madirex.funkosspringrest.repositories;
 
 import com.madirex.funkosspringrest.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 /**
  * Interface FunkoRepository
  */
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long>, JpaSpecificationExecutor<Category> {
 
 }
