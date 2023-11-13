fa1c2ec48f4ee75c65362c6a2995bd13c9efee81
Madirex <madirexland@gmail.com>
2023-11-13 14:47:16 +0100

    FileStorage tests fixed

--- src/test/java/com/madirex/funkosspringrest/storage/FileSystemStorageServiceTest.java
+++ src/test/java/com/madirex/funkosspringrest/storage/FileSystemStorageServiceTest.java
@@ -18,12 +18,12 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class FileSystemStorageServiceTest {
@@ -34,12 +34,19 @@ class FileSystemStorageServiceTest {
     @InjectMocks
     private final FileSystemStorageService fileSystemStorageService = new FileSystemStorageService("funkos-images");
 
+    byte[] bytesPNG = { (byte) 137, (byte) 80, (byte) 78, (byte) 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82,
+            0, 0, 0, 1, 0, 0, 0, 1, 8, 6, 0, 0, 0, 31, 21, (byte) -60, (byte) -60,
+            (byte) 137, (byte) 80, (byte) 78, (byte) 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82,
+            0, 0, 0, 1, 0, 0, 0, 1, 8, 6, 0, 0, 0, 31, 21, (byte) -60, (byte) -60 };
+
     @BeforeEach
     public void setUp() throws IOException {
         fileSystemStorageService.deleteAll();
         fileSystemStorageService.init();
-        fileSystemStorageService.store(new MockMultipartFile("funko", "funko.jpg", "image/jpeg", "funko".getBytes()));
-        fileSystemStorageService.store(new MockMultipartFile("funko", "funko2.jpg", "image/jpeg", "funko".getBytes()));
+        fileSystemStorageService.store(new MockMultipartFile("funko", "funko.png",
+                "image/png", bytesPNG), List.of("jpg", "jpeg", "png"), UUID.randomUUID().toString());
+        fileSystemStorageService.store(new MockMultipartFile("funko", "funko2.png",
+                "image/png", bytesPNG), List.of("jpg", "jpeg", "png"), UUID.randomUUID().toString());
         MockHttpServletRequest request = new MockHttpServletRequest();
         ServletRequestAttributes attrs = new ServletRequestAttributes(request);
         RequestContextHolder.setRequestAttributes(attrs);
@@ -47,25 +54,34 @@ class FileSystemStorageServiceTest {
 
     @Test
     void testStore() throws IOException {
-        String file = fileSystemStorageService.store(new MockMultipartFile("funko", "funko.jpg", "image/jpeg", "funko".getBytes()));
+        var id = UUID.randomUUID().toString();
+        String file = fileSystemStorageService.store(new MockMultipartFile("funko", "funko.png",
+                "image/png", bytesPNG), List.of("jpg", "jpeg", "png"), id);
 
         assertAll(
                 () -> assertNotNull(file),
-                () -> assertTrue(file.contains("funko.jpg"))
+                () -> assertTrue(file.contains(id + ".png"))
         );
     }
 
     @Test
     void testStoreWithEmptyFile() {
-        var res = assertThrows(StorageBadRequest.class, () -> fileSystemStorageService.store(new MockMultipartFile("funko", "funko.jpg", "image/jpeg", "".getBytes())));
-        assertEquals("Fichero vacío funko.jpg", res.getMessage());
+        var res = assertThrows(StorageBadRequest.class, () -> fileSystemStorageService.store(
+                new MockMultipartFile("funko", "funko.png",
+                        "", "".getBytes()),
+                List.of("jpg", "jpeg", "png"), UUID.randomUUID().toString()));
+        assertEquals("Fichero vacío funko.png", res.getMessage());
     }
 
 
     @Test
     void testStoreWith2puntos() {
-        var res = assertThrows(StorageBadRequest.class, () -> fileSystemStorageService.store(new MockMultipartFile("funko", "../funko.jpg", "image/jpeg", "funko".getBytes())));
-        assertEquals("No se puede almacenar un fichero con una ruta relativa fuera del directorio actual ../funko.jpg", res.getMessage());
+        var res = assertThrows(StorageBadRequest.class, () -> fileSystemStorageService.store(
+                new MockMultipartFile("funko", "../funko.png",
+                        "image/png", bytesPNG),
+                List.of("jpg", "jpeg", "png"), UUID.randomUUID().toString()));
+        assertEquals("No se puede almacenar un fichero con una ruta relativa fuera del " +
+                "directorio actual ../funko.png", res.getMessage());
     }
 
     @Test
@@ -79,39 +95,43 @@ class FileSystemStorageServiceTest {
 
     @Test
     void testLoad() {
-        var res = fileSystemStorageService.load("funko.jpg");
+        var res = fileSystemStorageService.load("funko.png");
         assertAll(
                 () -> assertNotNull(res),
-                () -> assertTrue(res.getFileName().toString().contains("funko.jpg"))
+                () -> assertTrue(res.getFileName().toString().contains("funko.png"))
         );
     }
 
     @Test
     void testLoadAsResource() throws IOException {
-        var file = fileSystemStorageService.store(new MockMultipartFile("funko", "funko.jpg", "image/jpeg", "funko".getBytes()));
+        var id = UUID.randomUUID().toString();
+        var file = fileSystemStorageService.store(
+                new MockMultipartFile("funko", "funko.png",
+                        "image/png", bytesPNG),
+                List.of("jpg", "jpeg", "png"), id);
         var res = fileSystemStorageService.loadAsResource(file);
         assertAll(
                 () -> assertNotNull(res),
-                () -> assertTrue(Objects.requireNonNull(res.getFilename()).contains("funko.jpg"))
+                () -> assertTrue(Objects.requireNonNull(res.getFilename()).contains(id + ".png"))
         );
     }
 
     @Test
     void testLoadAsResoureNotFound() {
-        var res = assertThrows(StorageNotFound.class, () -> fileSystemStorageService.loadAsResource("funko2.jpg"));
+        var res = assertThrows(StorageNotFound.class, () -> fileSystemStorageService.loadAsResource("funko.png"));
         assertAll(
                 () -> assertNotNull(res),
-                () -> assertEquals("No se puede leer el fichero: funko2.jpg", res.getMessage())
+                () -> assertEquals("No se puede leer el fichero: funko.png", res.getMessage())
         );
     }
 
     @Test
     void testLoadAsResoureMalformedUrl() {
-        var res = assertThrows(StorageNotFound.class, () -> fileSystemStorageService.loadAsResource("funko2.jpg"));
+        var res = assertThrows(StorageNotFound.class, () -> fileSystemStorageService.loadAsResource("funko.png"));
 
         assertAll(
                 () -> assertNotNull(res),
-                () -> assertEquals("No se puede leer el fichero: funko2.jpg", res.getMessage())
+                () -> assertEquals("No se puede leer el fichero: funko.png", res.getMessage())
         );
     }
 
@@ -149,7 +169,10 @@ class FileSystemStorageServiceTest {
 
     @Test
     void testDelete() throws IOException {
-        var file = fileSystemStorageService.store(new MockMultipartFile("funko", "funko.jpg", "image/jpeg", "funko".getBytes()));
+        var file = fileSystemStorageService.store(
+                new MockMultipartFile("funko", "funko.png",
+                        "image/png", bytesPNG),
+                List.of("jpg", "jpeg", "png"), UUID.randomUUID().toString());
         fileSystemStorageService.delete(file);
         var res = fileSystemStorageService.loadAll().toList();
         assertAll(
