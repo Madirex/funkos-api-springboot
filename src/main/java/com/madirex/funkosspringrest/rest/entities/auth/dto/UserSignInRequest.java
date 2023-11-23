5b40cf6a55a11a5191bcb5fd179878a525c00375
Madirex <madirexland@gmail.com>
2023-11-23 19:11:43 +0100

    Added auth DTO

---
+++ src/main/java/com/madirex/funkosspringrest/rest/entities/auth/dto/UserSignInRequest.java
@@ -0,0 +1,24 @@
+package com.madirex.funkosspringrest.rest.entities.auth.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+/**
+ * UserSignInRequest
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSignInRequest {
+    @NotBlank(message = "Username no puede estar vacío")
+    private String username;
+
+    @NotBlank(message = "Password no puede estar vacío")
+    @Length(min = 5, message = "Password debe tener al menos 5 caracteres")
+    private String password;
+}
