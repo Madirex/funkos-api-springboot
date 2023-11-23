9b6b03cf9a849af13bd7b4b623a380cbfe51126b
Madirex <madirexland@gmail.com>
2023-11-23 13:35:22 +0100

    Added queries to OrderRepository

--- src/main/java/com/madirex/funkosspringrest/rest/entities/order/repository/OrderRepository.java
+++ src/main/java/com/madirex/funkosspringrest/rest/entities/order/repository/OrderRepository.java
@@ -5,9 +5,26 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * OrderRepository
  */
 @Repository
 public interface OrderRepository extends MongoRepository<Order, ObjectId> {
+    /**
+     * Encuentra los pedidos dado un ID de usuario
+     *
+     * @param userId ID de usuario
+     * @return Lista de pedidos
+     */
+    List<Order> findOrdersByUserId(String userId);
+
+    /**
+     * Existe un producto con el ID de usuario
+     *
+     * @param userId Id de usuario
+     * @return ¿producto existe con el ID de usuario?
+     */
+    boolean existsByUserId(String userId);
 }
