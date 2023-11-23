965bfe085a93e52241d0e88c587492819ef15653
Madirex <madirexland@gmail.com>
2023-11-23 13:34:08 +0100

    User mapper implementation

---
+++ src/main/java/com/madirex/funkosspringrest/rest/entities/user/mappers/UsersMapper.java
@@ -0,0 +1,92 @@
+package com.madirex.funkosspringrest.rest.entities.user.mappers;
+
+import com.madirex.funkosspringrest.rest.entities.user.dto.UserInfoResponse;
+import com.madirex.funkosspringrest.rest.entities.user.dto.UserRequest;
+import com.madirex.funkosspringrest.rest.entities.user.dto.UserResponse;
+import com.madirex.funkosspringrest.rest.entities.user.models.User;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Clase UsersMapper
+ */
+@Component
+public class UsersMapper {
+    /**
+     * Método que convierte un objeto UserRequest en un objeto User
+     *
+     * @param request UserRequest
+     * @return User
+     */
+    public User toUser(UserRequest request) {
+        return User.builder()
+                .name(request.getName())
+                .surname(request.getSurname())
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(request.getPassword())
+                .roles(request.getRoles())
+                .isDeleted(request.getIsDeleted())
+                .build();
+    }
+
+    /**
+     * Método que convierte un objeto UserRequest y un UUID en un objeto User
+     *
+     * @param request UserRequest
+     * @param id      ID del usuario
+     * @return User
+     */
+    public User toUser(UserRequest request, UUID id) {
+        return User.builder()
+                .id(id)
+                .name(request.getName())
+                .surname(request.getSurname())
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(request.getPassword())
+                .roles(request.getRoles())
+                .isDeleted(request.getIsDeleted())
+                .build();
+    }
+
+    /**
+     * Método que convierte un objeto User en un objeto UserResponse
+     *
+     * @param user usuario
+     * @return UserResponse
+     */
+    public UserResponse toUserResponse(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .surname(user.getSurname())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .roles(user.getRoles())
+                .isDeleted(user.getIsDeleted())
+                .build();
+    }
+
+    /**
+     * Método que convierte un objeto User y una lista de pedidos en un objeto UserInfoResponse
+     *
+     * @param user   usuario
+     * @param orders pedidos
+     * @return UserInfoResponse
+     */
+    public UserInfoResponse toUserInfoResponse(User user, List<String> orders) {
+        return UserInfoResponse.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .surname(user.getSurname())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .roles(user.getRoles())
+                .isDeleted(user.getIsDeleted())
+                .orders(orders)
+                .build();
+    }
+}
