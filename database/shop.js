a269bea44c947c52a7e50d8133a4d797c9d2cc9e
Madirex <madirexland@gmail.com>
2023-11-25 12:21:49 +0100

    changed data.sql and shop.js

--- database/shop.js
+++ database/shop.js
@@ -1,6 +1,6 @@
 db.orders.insertMany([
     {
-        userId: "1",
+        userId: "a2bcb7b3-4c36-4073-9ab8-9d8f4a57301b",
         client: {
             fullName: "John Doe",
             email: "john.doe@example.com",
@@ -17,20 +17,20 @@ db.orders.insertMany([
         orderLineList: [
             {
                 quantity: 2,
-                productId: "product1",
+                productId: "57b77805-c1ce-490f-a96f-ec15505d5fae",
                 productPrice: 19.99,
                 total: 39.98
             },
             {
                 quantity: 1,
-                productId: "product2",
+                productId: "874f872f-c30e-4089-ab33-16fd1c4d4344",
                 productPrice: 24.99,
                 total: 24.99
             }
         ]
     },
     {
-        userId: "2",
+        userId: "a2bcb7b3-4c36-4073-9ab8-9d8f4a57301b",
         client: {
             fullName: "Jane Doe",
             email: "jane.doe@example.com",
@@ -47,7 +47,7 @@ db.orders.insertMany([
         orderLineList: [
             {
                 quantity: 3,
-                productId: "product3",
+                productId: "57b77805-c1ce-490f-a96f-ec15505d5fae",
                 productPrice: 29.99,
                 total: 89.97
             }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,9 +7,9 @@ VALUES (1, 'DISNEY', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, true),
 
 INSERT INTO Funko (id, name, price, quantity, image, category_id, created_at, updated_at)
 VALUES
-    (UUID(), 'Mickey Mouse', 19.99, 10, 'https://www.madirex.com/funko/mickeymouse', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    (UUID(), 'Harry Potter', 24.99, 8, 'https://www.madirex.com/funko/harrypotter', 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    (UUID(), 'Stranger Things', 29.99, 5, 'https://www.madirex.com/funko/strangerthings', 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('57b77805-c1ce-490f-a96f-ec15505d5fae', 'Mickey Mouse', 19.99, 10, 'https://www.madirex.com/funko/mickeymouse', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('874f872f-c30e-4089-ab33-16fd1c4d4344', 'Harry Potter', 24.99, 8, 'https://www.madirex.com/funko/harrypotter', 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('8919f2e4-193b-4928-b4d4-5e5fb88653e7', 'Stranger Things', 29.99, 5, 'https://www.madirex.com/funko/strangerthings', 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
     (UUID(), 'Spider-Man', 14.99, 12, 'https://www.madirex.com/funko/spiderman', 4, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
     (UUID(), 'Monkey D. Luffy', 34.99, 7, 'https://www.madirex.com/funko/monkeydluffy', 5, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
     (UUID(), 'Doctor Who', 39.99, 3, 'https://www.madirex.com/funko/doctorwho', 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
